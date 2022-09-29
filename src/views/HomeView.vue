e659c136989c34c1ef8bf337288bb8d37e20600d
魏大炮 <3378923215@qq.com>
2022-09-29 10:23:26 +0800

    feat(custom): 配置跨域

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "",
+  components: {},
+  data() {
+    return {
+      username: "admin",
+      password: "123456",
+      code: "we3r",
+    };
+  },
+  created() {
+    this.$axios
+      .post(
+        "http://119.45.133.128:8089/api/user/login?username=this.username&password=this.password&code=this.code"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  mounted() {},
+  methods: {},
+};
 </script>
+<style scoped lang='scss'>
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8888,
+    host: "localhost",
+    open: false,
+    proxy: {
+      // 代理名称
+      [process.env.VUE_APP_BASE_API]: {
+        // 跨域地址
+        target: process.env.VUE_APP_SERVICE_URL,
+        // 是否开启跨域
+        changeOrigin: true,
+        // 重写路径
+        pathRewrite: {
+          ["^" + process.env.VUE_APP_BASE_API]: ""
+        }
+      }
+    }
+  }
 })
